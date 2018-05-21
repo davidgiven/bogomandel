83065f7dea6b48a8459b3e0e3b3707051dc5212c
David Given <dg@cowlark.com>
2018-05-21 13:32:44 +0200

    Recursive box drawing works.

--- mandel.asm
+++ mandel.asm
@@ -7,21 +7,30 @@ screeny:    .byte 0
 
 pixelcol:   .byte 0
 pixelmask:  .byte 0
+temp:       .byte 0
 
-boxx:       .byte 0
-boxy:       .byte 0
-boxsize:    .byte 0
+boxx1:      .byte 0
+boxy1:      .byte 0
+boxx2:      .byte 0 ; INCLUSIVE (so 0..255)
+boxy2:      .byte 0 ; INCLUSIVE (so 0..255)
+midx:       .byte 0
+midy:       .byte 0
 sidecount:  .byte 0
 
 xc:         .byte 0
 
+#define PUSH(var) lda var : pha
+#define POP(var) pla : sta var
+
 .text
     * = $0e00
     jsr setup
-    stz boxx
-    stz boxy
+    lda #0
+    sta boxx1
+    sta boxy1
     lda #255
-    sta boxsize
+    sta boxx2
+    sta boxy2
     jsr box
     rts
 
@@ -30,67 +39,159 @@ box:
     ; The line drawing routines don't draw the last pixel, so do that
     ; specially. (We need to probe one pixel anyway so it's no bad
     ; thing.)
-    lda boxsize
-    lsr
-    clc
-    adc boxx
+    lda boxx2
     sta screenx
-    lda boxsize
-    clc
-    adc boxy
+    lda boxy2
     sta screeny
     jsr calculate_screen_address
     jsr calculate
 
     ; Top stroke
-    lda boxx
+
+    lda boxx1
     sta screenx
-    lda boxy
+    lda boxy1
     sta screeny
-    lda boxsize
+    lda boxx2
+    sec
+    sbc boxx1
     lsr
     sta sidecount
     jsr calculate_screen_address
     jsr hline
 
     ; Right stroke
+
     ; screenx, screeny preserved
-    lda boxsize
+    lda boxy2
+    sec
+    sbc boxy1
     sta sidecount
     jsr calculate_screen_address
     jsr vline
 
     ; Bottom stroke
-    ; screeny preserved
-    lda boxx
+
+    lda boxy2
+    sta screeny
+    lda boxx1
     sta screenx
-    lda boxsize
+    lda boxx2
+    sec
+    sbc boxx1
     lsr
     sta sidecount
     jsr calculate_screen_address
     jsr hline
 
     ; Left stroke
-    lda boxx
+
+    lda boxx1
     sta screenx
-    lda boxy
-    inc
+    lda boxy1
     sta screeny
-    lda boxsize
-    dec
+    lda boxy2
+    sec
+    sbc boxy1
     sta sidecount
     jsr calculate_screen_address
     jsr vline
 
-exit:
+    ; Start recursion.
+
+    PUSH(midx)
+    PUSH(midy)
+
+    ; Calculate centre point.
+
+    lda boxx1
+    lsr
+    sta temp
+    lda boxx2
+    lsr
+    clc
+    adc temp
+    and #$fe ; round down
+    sta midx
+    cmp boxx1
+    beq dont_recurse
+    
+    lda boxy1
+    lsr
+    sta temp
+    lda boxy2
+    lsr
+    clc
+    adc temp
+    sta midy
+    cmp boxy1
+    beq dont_recurse
+
+    ; Recurse into top left.
+
+    PUSH(boxx2)
+    PUSH(boxy2)
+
+    lda midx
+    sta boxx2
+    lda midy
+    sta boxy2
+    jsr box
+
+    POP(boxy2)
+    POP(boxx2)
+
+    ; Recurse into bottom left.
+
+    PUSH(boxx2)
+    PUSH(boxy1)
+    
+    lda midx
+    sta boxx2
+    lda midy
+    sta boxy1
+    jsr box
+
+    POP(boxy1)
+    POP(boxx2)
+
+    ; Recurse into top right.
+
+    PUSH(boxx1)
+    PUSH(boxy2)
+
+    lda midx
+    sta boxx1
+    lda midy
+    sta boxy2
+    jsr box
+
+    POP(boxy2)
+    POP(boxx1)
+
+    ; Recurse into bottom right.
+
+    PUSH(boxx1)
+    PUSH(boxy1)
+
+    lda midx
+    sta boxx1
+    lda midy
+    sta boxy1
+    jsr box
+
+    POP(boxy1)
+    POP(boxx1)
+
+dont_recurse:
+    POP(midy)
+    POP(midx)
+
     rts
 
 calculate:
-    jsr pick
-    lsr pixelcol
-    lsr pixelcol
-    lsr pixelcol
-    lsr pixelcol
+    lda palette+7
+    sta pixelcol
     jsr plot
     rts
 
@@ -128,13 +229,11 @@ calculate_screen_address:
     sta screenptr+1
 
     ; Calculate the X offset (into pixelmask/A)
-    lda screenx
-    and #$fe        ; mask off even/odd pixels; range is 0..159
+    lda screenx     ; remember these are logical pixels (0..255)
+    and #$fc        ; mask to char column
     stz pixelmask   ; reuse this as high byte of X offset
     asl             ; *2
     rol pixelmask
-    asl             ; *4
-    rol pixelmask   ; clears C
     
     ; Add on the X offset.
     ; C is already clear
@@ -183,13 +282,15 @@ row_table:
     row_addr(31)
 .)
 
-; Given a calculated screenptr, moves to the next horizontal pixel.
+; Given a calculated screenptr, moves to the next horizontal physical pixel
+; (which is two logical pixels because MODE 2).
 go_to_pixel_right:
 .(
     lda screenx
     inc
+    inc
     sta screenx
-    and #1
+    and #3
     bne exit
 
     clc
@@ -238,7 +339,8 @@ plot:
     ; Unshifted values refer to the *left* hand pixel, so odd pixels
     ; need adjusting.
     lda screenx     ; Is this an even pixel?
-    ror             ; low bit to C
+    ror
+    ror             ; odd/even bit to C
     bcc even_pixel
 
     lsr pixelcol
@@ -257,7 +359,8 @@ even_pixel:
 pick:
 .(
     lda screenx
-    ror             ; low bit into carry
+    ror
+    ror             ; odd/even bit to C
     lda (screenptr)
     ; Unshifted values refer to the *left* hand pixel, so odd pixels
     ; need adjusting.
